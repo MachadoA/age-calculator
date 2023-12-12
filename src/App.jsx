e990c3f46bd2b04d0eef75a4aa8072ae78817a44
devMachadoA <amachadoamaral2@gmail.com>
2023-12-12 12:49:56 +0000

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Calculator from './components/Calculator'
-
 import './App.css'
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
       <Calculator/>
       <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="#">Your Name Here</a>.
+        Coded by <a href="#">Ana Amaral</a>.
     </div>
     </>
   )
